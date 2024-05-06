834cdcf78e6657e1f565905ea064c79d8a6c4720
spectre <e.sahinnn61@gmail.com>
2024-05-06 18:58:51 +0200

    for loops

---
+++ for loops/index.js
@@ -0,0 +1,11 @@
+export function totalBirdCount(birdsPerDay) {
+  throw new Error("Please implement the totalBirdCount function");
+}
+
+export function birdsInWeek(birdsPerDay, week) {
+  throw new Error("Please implement the birdsInWeek function");
+}
+
+export function fixBirdCountLog(birdsPerDay) {
+  throw new Error("Please implement the fixBirdCountLog function");
+}
