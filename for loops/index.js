dc364401190259fca0b05e0176aeb827a90cd561
spectre <e.sahinnn61@gmail.com>
2024-05-07 00:48:12 +0200

    fix

--- for loops/index.js
+++ for loops/index.js
@@ -1,11 +1,27 @@
 export function totalBirdCount(birdsPerDay) {
-  throw new Error("Please implement the totalBirdCount function");
+  let total = 0;
+  for (let i = 0; i < birdsPerDay.length; i++) {
+    total = total + birdsPerDay[i];
+  }
+  return total;
 }
 
 export function birdsInWeek(birdsPerDay, week) {
-  throw new Error("Please implement the birdsInWeek function");
-}
+  let startIndex = (week - 1) * 7; // calcul de l'indice de départ
+  let endIndex = startIndex + 6; // calcul de l'indice de fin
+  let total = 0; // variable pour stocker le total des oiseaux
 
+  // boucle pour sommer les éléments du tableau
+  for (let i = startIndex; i <= endIndex; i++) {
+    total += birdsPerDay[i];
+  }
+
+  return total; // retourner le total des oiseaux
+}
 export function fixBirdCountLog(birdsPerDay) {
-  throw new Error("Please implement the fixBirdCountLog function");
+  for (let i = 0; i < birdsPerDay.length; i++) {
+    birdsPerDay[i] += 1;
+    i++;
+  }
+  return birdsPerDay;
 }
